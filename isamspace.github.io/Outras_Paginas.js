16402f9235c9b332e6febfa1b9e3b89019da3e5c
IsamSpace <155160846+IsamSpace@users.noreply.github.com>
2024-01-05 14:59:17 -0300

    Delete isamspace.github.io directory

--- isamspace.github.io/Outras_Paginas.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
-
-function fecharMenu() {
-    var menu = document.getElementById("menu");
-    menu.style.display = "none";
-}
